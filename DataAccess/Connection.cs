cb2573a637e1814b1532ee55cadaf6eb1b783b26
Javad <Javad@DESKTOP-K19MC5C>
2017-07-10 12:08:50 +0430

    hi

---
+++ DataAccess/Connection.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess
+{
+    class Connection
+    {
+        public SchoolDBEntities GetContext()
+        {
+            SchoolDBEntities sd = new SchoolDBEntities();
+            return sd;
+        }
+    }
+}
---
+++ DataAccess/Repository/StudentRepository.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Repository
+{
+    public class StudentRepository
+    {
+        private Connection conn;
+
+        public StudentRepository()
+        {
+            conn = new Connection();
+        }
+        public Student FindByNatinalCode(int nationalCode)
+        {
+            Student result = null;
+
+            using (SchoolDBEntities sd  = conn.GetContext())
+            {
+                //--  SELECT * FROM vPhoneList WHERE PhobeID = phoneID
+
+                result = (from r in sd.Students
+                          where r.NationalCode == nationalCode
+                          select r).FirstOrDefault();
+            }
+
+            return result;
+        }
+        
+    }
+
+}
